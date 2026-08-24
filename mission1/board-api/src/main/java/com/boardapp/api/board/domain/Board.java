2c1b8717c9078efd830d15e6ef2d84657fa1189e
osc-minhee <minnie.kim@osckorea.com>
2026-08-24 17:56:53 +0900

    REFACTOR: Base Entity 추가

--- mission1/board-api/src/main/java/com/boardapp/api/board/domain/Board.java
+++ mission1/board-api/src/main/java/com/boardapp/api/board/domain/Board.java
@@ -1,37 +1,20 @@
 package com.boardapp.api.board.domain;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.boardapp.api.global.entity.BaseEntity;
 
 import lombok.Getter;
 
 @Entity
 @Table(name = "boards")
-@EntityListeners(AuditingEntityListener.class)
 @Getter
-public class Board {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Board extends BaseEntity {
 
     private String title;
 
     private String content;
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
 
     protected Board() {
     }
